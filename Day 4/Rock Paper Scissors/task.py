288e0fd8e76d799bd27300ab408bce68aaa44539
KWolf <KWolf484@users.noreply.github.com>
2024-09-28 22:47:40 +0100

    RPS - Base game written

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,5 +1,46 @@
-from art import rock, paper ,scissors
+import random
+#imports ASCII art for RPS from ./art.py
+from art import rock, paper, scissors
 
+#Constatent variables
+ROCK = 0
+PAPER = 1
+SCISSORS = 2
 
+#Ask player to make a choice between RPS
+player = input("Choose: (R)ock, (P)aper or (S)cissors.\n").upper()
 
+#Assigns a random (0 1 2) integer to variable comp
+comp = random.randint(0, 2)
 
+"""Assigns player choice to corresponding number R=0 P=1 S=2"""
+if player == "R" or player == "ROCK":
+    player = ROCK
+elif player == "P" or player == "PAPER":
+    player = PAPER
+elif player == "S" or player == "SCISSORS":
+    player = SCISSORS
+else:
+    print("Player input wasn't recognized, try again")
+
+def win_lose(p, c):
+    """Take two args (p = player, c = comp) and evaluated who wins"""
+    if p == c:
+        print("Draw")
+    elif p == 0 and c == 2 or p == 1 and c == 0 or p == 2 and c == 1:
+        print("You Win")
+    else:
+        print("You Lose")
+
+def ascii_art(x):
+    """prints ASCII art"""
+    if x == 0:
+        return rock
+    elif x == 1:
+        return paper
+    elif x == 2:
+        return scissors
+
+print(f"Player:\n{ascii_art(x=player)}")
+print(f"Computer:\n{ascii_art(x=comp)}")
+win_lose(p=player, c=comp)
