3a3d3dc57050da823991f32212924362c53b896a
KWolf <KWolf484@users.noreply.github.com>
2024-10-01 23:07:38 +0100

    setup on ProjEvo and progress.

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,7 @@
+def greet(name):
+    print(f"Hello {name}")
+    print(f"Hi, my name is {name} ")
+    print(f"{name} said, 'Ello, to the clerk")
+
+greet(name="Kris")
+
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -4,5 +4,11 @@ def greet_with_name(name):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
 
-
 greet_with_name("Jack Bauer")
+
+
+
+def multi_input_greeting(name, age, location):
+    print(f"Hi, my name is {name}\nI'm from {location}\nand I am {age} years old")
+
+multi_input_greeting(name="Derk", age="45", location="England")
