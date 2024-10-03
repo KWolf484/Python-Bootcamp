d65f42265eab8e465cafc3d4debd7ff893cefbbf
KWolf <KWolf484@users.noreply.github.com>
2024-10-03 15:18:58 +0100

    Progress - Day 9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -2,5 +2,23 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+bid = True
+while bid:
+
+    username = input("Enter the bidders name: ")
+    user_bid = int(input("Enter your bid: "))
+    cont_bid = input("Continue bidding?\n(Y)es or (N). ").lower()
+    auction_bids = {username: user_bid}
+    if cont_bid == "no" or cont_bid == "n":
+        bid = False
 
 
+
+
+top_bid = {}
+for key in auction_bids:
+    for value in auction_bids[key]:
+        if value > top_bid:
+            top_bid = {key: value}
+
+print(top_bid)
