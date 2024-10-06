e392e62f80d91900c0be7b12106fd83d5544be29
KWolf <KWolf484@users.noreply.github.com>
2024-10-06 23:06:43 +0100

    Bj update, reconfigured functions, started game flow

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,45 +1,80 @@
 import random
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-comp = []
-user = []
+comp_hand = []
+user_hand = []
 
-def draw(player, num_cards):
+def draw(player_hand, num_cards):
     """Take two arguments, player name and number of cards to be drawn.
        passes players cards to calc_hand func """
     for card in range(num_cards):
-    ### adds the given number of cards to the given player ###
-        player.append(random.choice(cards))
-    ### returns given players hand to the calc_hand func, ###
-    ### which will calc and store hands current value     ###
-    return calc_hand(player)
+        player_hand.append(random.choice(cards))
+    return calc_hand(player_hand)
 
-def calc_hand(player):
+def calc_hand(player_hand):
     """Calc the given players hands, returns the hand_score """
     hand_score = 0
-### Iterates through players cards ###
-    for card in player:
-    ### adds each cards value to hand_score's total ###
+    for card in player_hand:
         hand_score += card
-    ### returns hand_score
     return hand_score
 
-def is_bust(player, player_score):
-    """Check to see if player is bust,
-    and if player is holding an Ace """
-    if len(player) > 2 and player_score > 21:
-        for card in player:
-            if card == 11:
-                card = 1
-                return player
-            else:
-                return True
-    elif len(player) == 2 and player_score == 21:
+def bust_or_bj(player_hand, player_score):
+    """Check to see if player is bust (True),
+    or has blackjack(-1)"""
+    if len(player_hand) == 2 and player_score == 21:
         return -1 # f"{player} Drew {player[0]} and {player[1]},\n BLACK JACK!"
     else:
+        if player_hand > 21:
+            return True
+        elif player_score <=21:
+            return False
+
+def has_ace(player_hand, player_score):
+    if len(player_hand) > 2 and player_score > 21:
+        return True
+    else:
         return False
+
+def display(player_hand):
+    """take a list (player) """
+    num_cards = len(player_hand)
+    if player_hand == comp_hand and num_cards == 2:
+        return f"{player_hand[0:num_cards - 1]} X"
+    else:
+        return f"{player_hand[0:num_cards]}"
+
 def game():
-    user_score = draw(player=user, num_cards=2)
-    comp_score = draw(player=comp, num_cards=2)
+    cont_game = True
+    user_score = draw(player_hand=user_hand, num_cards=2)
+    comp_score = draw(player_hand=comp_hand, num_cards=2)
+    while cont_game:
+        if bust_or_bj(user_hand, user_score) == -1:
+            cont_game = False
+            print(f"")
+
+        if not bust_or_bj(comp_hand, comp_score) == -1:
+            if bust_or_bj(comp_hand, comp_score):
+                cont_game = False
+                print(f"Computer bust with {comp_score}\n Player wins")
+
+            while comp_score < 17 and len(comp_hand) < 5 and bust_or_bj(comp_hand, comp_score) != True:
+                draw(comp_hand, comp_score)
+                bust_or_bj(comp_hand, comp_score)
+            else:
+                if not bust_or_bj(comp_hand, comp_score):
+                    cont_game = False
+
+
+
+    else:
+        return
+
+
+
+
+
+    print(display(comp_hand))
+    print(display(user_hand))
+game()
 
 
